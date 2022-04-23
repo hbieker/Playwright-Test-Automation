1fe47ed82bf2164e25429525a4e46c8721fce933
Heath Bieker <hbieker@gmail.com>
2022-04-23 16:04:02 -0600

    Initial Commit

---
+++ tests/test_structure.spec.js
@@ -0,0 +1,20 @@
+//Import Playwright Module
+const {test} = require('@playwright/test');
+
+//Test Annotation takes 2 arguments - test case name & test function
+//Marking function with async to allow for awaits
+//function() converted to ()=> for lighter code
+//Basic syntax for each test is as follows
+
+test('test_case_name', async ()=>
+{
+    //Playwright Code
+    //javascript is asynchronous
+    //must explicitly tell it to wait
+    await
+    //step 1
+    await
+    //step 2
+    await
+    //step 3
+});
